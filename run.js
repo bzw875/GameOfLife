57ad0b863453a0e492e07395a51d8c5cc4b5da58
班兆武 <banzhaowu@ausxin.com>
2017-08-10 22:46:40 +0800

    删除jquery的依赖，和增加测试

---
+++ run.js
@@ -0,0 +1,24 @@
+var assert = require('chai').assert;
+
+var expected, current;
+
+before(function(){
+    expected = ['a', 'b', 'c'];
+});
+
+
+describe('String#split', function(){
+    beforeEach(function(){
+        current = 'a,b,c'.split(',');
+    });
+    it('should return an array', function(){
+        assert(Array.isArray(current));
+    });
+
+    it('should return the same array', function(){
+        assert.equal(expected.length, current.length, 'array have equal length');
+        for (var i = 0; i < expected.length; i++) {
+            assert.equal(expected[i], current[i], i + 'element is equal');
+        }
+    })
+});
